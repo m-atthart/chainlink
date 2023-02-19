76631af94a67ce7c3043b27abca8a9744db13e93
Matthew Hart <mattevanhart@gmail.com>
2023-02-19 01:13:04 -0500

    change env var name

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,7 +7,7 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
 	DATABASE_URL: z.string().url(),
-	NODE_ENV: z.enum(["development", "test", "production"]),
+	ENV_TYPE: z.enum(["local", "development", "production"]),
 	NEXTAUTH_SECRET: z.string(),
 	NEXTAUTH_URL: z.string().url(),
 	TWITCH_CLIENT_ID: z.string(),
