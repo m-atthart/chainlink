3ac6b2823eb342de13c3c8fc719c928ae7993d5f
Matthew Hart <mattevanhart@gmail.com>
2023-04-02 01:05:30 -0400

    clerk with prebuilt components

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 
-export function middleware(request: NextRequest) {
+export default withClerkMiddleware((request: NextRequest) => {
 	if (request.nextUrl.pathname.startsWith("/user/")) {
 		const username = request.nextUrl.pathname.split("/")[2];
 		return NextResponse.rewrite(
@@ -13,8 +14,15 @@ export function middleware(request: NextRequest) {
 	) {
 		return NextResponse.rewrite(new URL("/user/me/chain", request.url));
 	}
-}
+
+	return NextResponse.next();
+});
 
 export const config = {
-	matcher: ["/", "/user", "/user/:username"],
+	matcher: [
+		"/",
+		"/user",
+		"/user/:username",
+		"/((?!_next/image|_next/static|favicon.ico).*)",
+	],
 };
