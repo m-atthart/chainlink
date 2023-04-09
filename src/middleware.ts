8e47d9480678eb74a113d28657324fd58a52cac6
Matthew Hart <mattevanhart@gmail.com>
2023-04-08 20:54:36 -0400

    change link structure

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,26 +3,13 @@ import type { NextRequest } from "next/server";
 import { withClerkMiddleware } from "@clerk/nextjs/server";
 
 export default withClerkMiddleware((request: NextRequest) => {
-	if (request.nextUrl.pathname.startsWith("/user/")) {
-		const username = request.nextUrl.pathname.split("/")[2];
-		return NextResponse.rewrite(
-			new URL(`/user/${username}/chain`, request.url)
-		);
-	} else if (
-		request.nextUrl.pathname === "/" ||
-		request.nextUrl.pathname === "/user"
-	) {
-		return NextResponse.rewrite(new URL("/user/me/chain", request.url));
+	if (request.nextUrl.pathname === "/") {
+		return NextResponse.redirect(new URL("/login", request.url));
 	}
 
 	return NextResponse.next();
 });
 
 export const config = {
-	matcher: [
-		"/",
-		"/user",
-		"/user/:username",
-		"/((?!_next/image|_next/static|favicon.ico).*)",
-	],
+	matcher: ["/", "/((?!_next/image|_next/static|favicon.ico).*)"],
 };
